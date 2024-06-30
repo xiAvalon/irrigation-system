70cf769b93fd877f9dbad72b8233f24ec221b932
xiAvalon <av.ammar7@gmail.com>
2024-06-30 09:56:02 +0300

    Server: use environment variables

--- irrigation-server/index.js
+++ irrigation-server/index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 const path = require('path');
+require('dotenv').config()
+
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || '5000';
 
 app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'work',
-    password: 'imAvalon76',
-    port: 5432,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'work',
+    password: process.env.DB_PASS || 'imAvalon76',
+    port: process.env.DB_PORT || '5432',
 });
 
 
